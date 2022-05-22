313f32c3674e4744a9ed9876ae3f969289c0e494
enjesh <enjesh@gmail.com>
2022-05-22 23:46:33 +0300

    You deserve to start your day right

---
+++ bestday.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#!/bin/bash
+
+# Just a simple script to kickstart your morning
+
+        echo "What is your name?"
+
+read name
+
+        echo "What is your favourite colour"
+
+read colour
+
+
+        echo "Good morning $name ❤ ❤ ❤"
+
+sleep 3
+
+        echo "You are looking awesome today in $colour 💥"
+
+sleep 3
+
+        echo "It is a beautiful day to be alive and well. Do enjoy it $name ‼"
